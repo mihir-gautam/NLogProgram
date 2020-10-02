f89b330a06b4cd9abae0fb912426073e431e91eb
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-02 19:56:29 +0530

    [Mihir] Add NLog implimentation to monitor logging in program

---
+++ AddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogProgram
+{
+    class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int SecondNum, int FirstNum)
+        {
+            if (FirstNum == 0 || SecondNum == 0)
+            {
+                nLog.LogDebug("Debug Unsuccessful : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("First Number & Second Number shouldnot be equal to 0");
+            }
+            int result = FirstNum + SecondNum;
+            nLog.LogDebug("Debug Successful : Sum()");
+            nLog.LogInfo("Sum method passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NLogProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition problem, enter two numbers to add");
+            int FirstNum = Convert.ToInt32(Console.ReadLine());
+            int SecondNum = Convert.ToInt32(Console.ReadLine());
+            AddNumbers add = new AddNumbers();
+            Console.WriteLine("Result is : " + add.Sum(FirstNum,SecondNum));
+        }
+    }
+}
